f4b4cbf597da5c294f6f46b555e7d554162252aa
Adrian Skogstad Pleym <adrian@Adrians-MacBook-Pro.local>
2020-11-12 10:47:48 +0100

    Done commenting the data generator file.

--- TDT4250Project.polet.model/src/polet/util/Constants.java
+++ TDT4250Project.polet.model/src/polet/util/Constants.java
@@ -5,6 +5,8 @@ public final class Constants {
     private Constants() {
             // restrict instantiation
     }
-
+    
+    // Type in your API key if you want to use the data generator
+    // and pull data from the server.
     public static final String API_KEY = "";
 }
--- TDT4250Project.polet.model/src/polet/util/DataGenerator.java
+++ TDT4250Project.polet.model/src/polet/util/DataGenerator.java
@@ -4,9 +4,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -41,54 +39,61 @@ import polet.Region;
 
 import static polet.util.Constants.API_KEY;
 
+
+// A class for generating data from our chosen API
 public class DataGenerator {
-	
-	private JsonArray editedData;
 	
 	public static void main(String[] args) {
 		DataGenerator dg = new DataGenerator();
-		dg.downloadJson();
-		dg.saveAsXMI();
+		//dg.downloadJson(); // If you uncomment this line, the generator will download the data from the server.
+		dg.saveAsXMI(); // This runs a function that will read a JSON file and create an XMI file.
 	}
 	
-	
+	// This method will download the json file.
 	private void downloadJson() {
         try
         {	
         	HttpClient httpclient = HttpClients.createDefault();
         	
-        	
-        	// Max 1048575
+        	// The next lines creates a HTTP request to the API server
             URIBuilder builder = new URIBuilder("https://apis.vinmonopolet.no/products/v0/details-normal");
-            builder.setParameter("maxResults", "1048575");
+            builder.setParameter("maxResults", "1048575"); // 1048575 is the maximum number we can pull, last checked 05.11.2020.
             java.net.URI uri = builder.build();
             
             HttpGet request = new HttpGet(uri);
             request.setHeader("Ocp-Apim-Subscription-Key", API_KEY);
-
+            
+            System.out.println("Downloading the database from the API.");
             HttpResponse response = httpclient.execute(request);
             HttpEntity entity = response.getEntity();
+            System.out.println("Success!");
+            System.out.println("-------------------------");
             
+            // Using a Json library to print a more readable Json structure
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
             if (entity != null) 
             {   
+            	System.out.println("Converting to string");
+            	
+            	// Creates a Json element from the string that is recieved from the server
                 JsonElement element = gson.fromJson(EntityUtils.toString(entity), JsonElement.class);
-                editedData = editedData(element.getAsJsonArray());
+                System.out.println("Filtering out unnecessary information and creates a json object.");
+                
+                // Here we call a function to structure the response from the http request.
+                // Otherwise we would have a lot of information that are not relevant for this program.
+                JsonArray editedData = editedData(element.getAsJsonArray());
                 String jsonString = gson.toJson(editedData);
-                // Write JSON file
-            	// Saves file in model folder
+                
+                // Write a JSON file
+            	// Saves file in ./model folder as data.json
                 try (FileWriter file = new FileWriter("./model/data.json")) {
-         
                     file.write(jsonString);
                     file.close();
-         
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                
             }
-           
         }
         catch (Exception e)
         {
@@ -96,14 +101,12 @@ public class DataGenerator {
         }
 	}
 	
+	// This is the function that will structure, filter and edit the data
+	// for our needs.
 	private JsonArray editedData(JsonArray jArray) {
 		
+		// This uses the GSON library for processing Json data.
 		JsonArray editedData = new JsonArray();
-		
-		//HashMap<String, String> mealtypes = new HashMap<String, String>();
-		
-		int counter = 0;
-		int limit = 500;
 		
 		for (JsonElement element : jArray) {
 			try {
@@ -157,40 +160,30 @@ public class DataGenerator {
 				x.add("mealTypes", mealtypes);
 				
 				editedData.add(x);
-				System.out.println(x.toString());
-				
-				counter += 1;
 				
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			if (counter == limit) {
-				break;
-			}
-			
 		}
 		
-		/*
-		Iterator it = mealtypes.entrySet().iterator();
-		while (it.hasNext()) {
-	        Map.Entry pair = (Map.Entry)it.next();
-	        System.out.println("Desc: " + pair.getKey() + " = " + "Id: " + pair.getValue());
-	        it.remove(); // avoids a ConcurrentModificationException
-		}
-		// Prints out number of elements in the database
-		*/
-		
-		System.out.println(editedData.size());
-		
+		System.out.println("Number of objects in the json file: " + editedData.size());
+		System.out.println("-------------------------");
 		
 		return editedData;	
 	}
 	
+	// The purpose of this method is to create an instance
+	// of our root object and objects from the data in the Json file.
 	private EObject jsonToXMI() {
 		Gson gson = new Gson();
 		JsonArray productArray;
+		
+		// Here we use an instance of the fatory for the model
+		// to create an instance of our root object, Categories.
 		Categories categories = PoletFactory.eINSTANCE.createCategories();
 		
+		// These hash maps are used for checking if an object
+		// is already made, and connect new object to those that already exist.
 		HashMap<String, Country> countries = new HashMap<String, Country>();
 		HashMap<String, Region> regions = new HashMap<String, Region>();
 		HashMap<String, ProductType> productTypes = new HashMap<String, ProductType>();
@@ -199,19 +192,25 @@ public class DataGenerator {
 		HashMap<String, ProductOfTypeInMealType> ptmts = new HashMap<String, ProductOfTypeInMealType>();
 		
 		try {
+		   int counter = 0;
+		   int limit = 500; // Here you can change how many products you want to generate for the XMI file.
 		  
-		   System.out.println("Reading JSON from a file");
-		   System.out.println("----------------------------");
+		   System.out.println("Reading JSON from file..");
+		   System.out.println("-------------------------");
 		   
+		   // Reads the stored Json file
 		   BufferedReader br = new BufferedReader(
 		     new FileReader("./model/data.json"));
 		   
-		   //convert the json string back to object
+		   // Converts the Json string back to an object
 		   productArray = gson.fromJson(br, JsonArray.class);
 		   
+		   // The loop will iterate through every valid product in the Json file,
+		   // create instances and relations after the model structure.
 		   for (JsonElement element : productArray) {
 			   Product product = PoletFactory.eINSTANCE.createProduct();
 			   
+			   // Product information
 			   product.setProductId(element.getAsJsonObject().get("product").getAsJsonObject().get("productId").getAsString());
 			   product.setName(element.getAsJsonObject().get("product").getAsJsonObject().get("name").getAsString());
 			   product.setPrice(element.getAsJsonObject().get("product").getAsJsonObject().get("price").getAsFloat());
@@ -220,6 +219,7 @@ public class DataGenerator {
 			   product.setPricePerAlcohol(element.getAsJsonObject().get("product").getAsJsonObject().get("pricePerAlcohol").getAsFloat());
 			   product.setPricePerVolume(element.getAsJsonObject().get("product").getAsJsonObject().get("pricePerVolume").getAsFloat());
 			   
+			   // Country
 			   Country country;
 			   
 			   if (!countries.containsKey(element.getAsJsonObject().get("country").getAsJsonObject().get("countryId").getAsString())) {
@@ -232,8 +232,11 @@ public class DataGenerator {
 				   country = countries.get(element.getAsJsonObject().get("country").getAsJsonObject().get("countryId").getAsString());
 			   }
 			   
+			   // Region
 			   Region region;
-			   
+			   // This one is a little more complicated.
+			   // Since some products haves the region name "Øvrige", which would be a region in every country.
+			   // We decided to register these products region name with the same country name to distinguish them. 
 			   if (((!regions.containsKey(element.getAsJsonObject().get("country").getAsJsonObject().get("region").getAsJsonObject().get("regionId").getAsString())) && !element.getAsJsonObject().get("country").getAsJsonObject().get("region").getAsJsonObject().get("regionId").getAsString().equals("00")) || (element.getAsJsonObject().get("country").getAsJsonObject().get("region").getAsJsonObject().get("regionId").getAsString().equals("00") && (!regions.containsKey(element.getAsJsonObject().get("country").getAsJsonObject().get("countryId").getAsString() + "1")))) {
 				   region = PoletFactory.eINSTANCE.createRegion();
 				   if (element.getAsJsonObject().get("country").getAsJsonObject().get("region").getAsJsonObject().get("regionId").getAsString().equals("00")) {
@@ -251,10 +254,11 @@ public class DataGenerator {
 				   } else {
 					   region = regions.get(element.getAsJsonObject().get("country").getAsJsonObject().get("region").getAsJsonObject().get("regionId").getAsString());
 				   }
-				   
 			   }
 			   
+			   // Product type
 			   ProductType pt;
+			   
 			   if (!productTypes.containsKey(element.getAsJsonObject().get("productType").getAsJsonObject().get("productTypeId").getAsString())) {
 				   pt = PoletFactory.eINSTANCE.createProductType();
 				   pt.setProductTypeId(element.getAsJsonObject().get("productType").getAsJsonObject().get("productTypeId").getAsString());
@@ -266,7 +270,9 @@ public class DataGenerator {
 			   }
 			   product.setProductType(pt);
 			   
+			   // Product of a type in region
 			   ProductOfTypeInRegion ptr;
+			   
 			   if (ptrs.containsKey(region.getRegionId() + pt.getProductTypeId())) {
 				   ptr = ptrs.get(region.getRegionId() + pt.getProductTypeId());
 			   } else {
@@ -277,6 +283,7 @@ public class DataGenerator {
 			   }
 			   ptr.getProducts().add(product);
 			   
+			   // A product can be suitable for multiple meal types
 			   List<MealType> mts = new ArrayList<>();
 			   
 			   for (JsonElement m : element.getAsJsonObject().get("mealTypes").getAsJsonArray()) {
@@ -293,6 +300,7 @@ public class DataGenerator {
 			   }
 			   
 			   for (MealType m : mts) {
+				   // Product of a type in meal type
 				   ProductOfTypeInMealType ptmt;
 				   if (ptmts.containsKey(pt.getProductTypeId() + m.getName()) ) {
 					   ptmt = ptmts.get(pt.getProductTypeId() + m.getName());
@@ -305,7 +313,12 @@ public class DataGenerator {
 				   ptmt.getProducts().add(product);
 			   }
 			   
-			   //System.out.println(categories.getProducts().toString());
+			   // The counter, to end the loop after a certain amount
+			   // of products are made.
+			   counter += 1;
+			   if (limit == counter) {
+				   break;
+			   }
 		   }
 		   
 		   
@@ -315,17 +328,22 @@ public class DataGenerator {
 		return categories;
 	}
 
+	// This function stores a file as an XMI type
 	private void saveAsXMI() {
+		// This uses the package information for our model
 		ResourceSet resSet = new ResourceSetImpl();
 		resSet.getPackageRegistry().put(PoletPackage.eNS_URI, PoletPackage.eINSTANCE);
 		resSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+		// The XMI resource will be created in the ./model folder as "categories.xmi"
 		Resource resource = resSet.createResource(URI.createFileURI("./model/categories.xmi"));
 		
+		// Reads the Json file an creates an EObject
 		resource.getContents().add(jsonToXMI());
 		try {
+			System.out.println("Generating the XMI file...");
 			resource.save(Collections.EMPTY_MAP);
+			System.out.println("Success!");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
